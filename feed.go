cccaf19d93ff8270d9bcf285c767c720b77d6a8d
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2013-03-19 14:12:11 -0400

    Use upstream xmlx now that it has merged.

--- feed.go
+++ feed.go
@@ -1,6 +1,6 @@
 /*
  Author: jim teeuwen <jimteeuwen@gmail.com>
- Dependencies: go-pkg-xmlx (http://github.com/anschelsc/go-pkg-xmlx)
+ Dependencies: go-pkg-xmlx (http://github.com/jteeuwen/go-pkg-xmlx)
 
  This package allows us to fetch Rss and Atom feeds from the internet.
  They are parsed into an object tree which is a hyvrid of both the RSS and Atom
@@ -28,7 +28,7 @@ package feeder
 import (
 	"errors"
 	"fmt"
-	xmlx "github.com/anschelsc/go-pkg-xmlx"
+	xmlx "github.com/jteeuwen/go-pkg-xmlx"
 	"net/http"
 	"strconv"
 	"strings"
