c6a78164358f4d6fe84e89a338395887a45c454f
Sean Schulte <sirsean@gmail.com>
2014-03-24 21:58:36 -0500

    Back to previous API.
    (IE, the previous breaking changes are unbroken.)

--- item.go
+++ item.go
@@ -16,7 +16,7 @@ type Item struct {
 	Comments    string
 	Enclosures  []*Enclosure
 	Guid        *string
-	PubDate     time.Time
+	PubDate     string
 	Source      *Source
 
 	// Atom specific fields
@@ -28,14 +28,18 @@ type Item struct {
 	Extensions map[string]map[string][]Extension
 }
 
+func (i *Item) ParsedPubDate() (time.Time, error) {
+    return parseTime(i.PubDate)
+}
+
 func (i *Item) Key() string {
 	switch {
 	case i.Guid != nil && len(*i.Guid) != 0:
 		return *i.Guid
 	case len(i.Id) != 0:
 		return i.Id
-	case len(i.Title) > 0 && !i.PubDate.IsZero():
-		return i.Title + i.PubDate.String()
+	case len(i.Title) > 0 && len(i.PubDate) > 0:
+		return i.Title + i.PubDate
 	default:
 		h := md5.New()
 		io.WriteString(h, i.Description)
